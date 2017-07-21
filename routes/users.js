f38237b74e31a3e122c920d190123863a3b6e1af
Matthew Hummer <biggreyson@gmail.com>
2017-07-21 10:29:08 -0600

    complete with vanilla javascript

--- routes/users.js
+++ routes/users.js
@@ -16,27 +16,42 @@ router.get('/' , (req, res, next) => {
 });
 
 router.post('/' , (req, res, next) => {
-  let firstName = req.body.users.firstName;
-  let lastName = req.body.users.lastName;
-  let username = req.body.users.username;
-  let email = req.body.users.email;
-  let phone = req.body.users.phone;
+  if (verify(req.body.users)){
+    let firstName = req.body.users.firstName;
+    let lastName = req.body.users.lastName;
+    let username = req.body.users.username;
+    let email = req.body.users.email;
+    let phone = req.body.users.phone;
 
-  knex('users')
-    .insert({
-      firstname: firstName,
-      lastname: lastName,
-      username: username,
-      email: email,
-      phone: phone
-    })
-    .returning(['firstname', 'lastname', 'username','phone','email'])
-    .then((results) => {
-      res.send(results[0]);
-    })
-    .catch((err) => {
-      next(err);
-    });
+    knex('users')
+      .insert({
+        firstname: firstName,
+        lastname: lastName,
+        username: username,
+        email: email,
+        phone: phone
+      })
+      .returning(['firstname', 'lastname', 'username','phone','email'])
+      .then((results) => {
+        res.send(results[0]);
+      })
+      .catch((err) => {
+        next(err);
+      });
+  }else{
+    res.sendStatus(400);
+  }
+
 });
+function verify({firstName, lastName, username, phone, email}){
+  let emailRegEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  let phoneRegEx = /^\d{10}$/;
+  if(!firstName || !lastName || !username|| !phone||!email ||!emailRegEx.test(email)||!phoneRegEx.test(phone)||username.length < 6||!/[a-zA-Z]/.test(username[0])||username.search(/[.,@<>(){}]/) != -1){
+    console.log('bad info');
+    return false
+  }
+  console.log('good info');
+  return true;
+}
 
 module.exports = router;
